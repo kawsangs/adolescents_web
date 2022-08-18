3706dfd6bacb431e6c02ac1b98d7a68e008cbc50
Sokly <hengsokly23@gmail.com>
2022-08-18 09:59:30 +0700

    About: add about page

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,8 @@ Rails.application.routes.draw do
 
   resource :about, only: [:show]
 
+  resource :about, only: [:show]
+
   if Rails.env.production?
     # Sidekiq
     authenticate :user, lambda { |u| u.primary_admin? } do
