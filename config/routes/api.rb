b473d9a38c3aadd7b46580fc336a0d14a30b9e7a
Sokly <hengsokly23@gmail.com>
2022-09-07 15:53:07 +0700

    Facility batch: import batch facilities from excel file

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :visits, only: [:create]
-      resources :app_users, only: [:create, :update]
+      resources :app_users, only: [:create, :update ]
       resources :facilities, only: [:index]
 
       get "*path" => "api#routing_error"
