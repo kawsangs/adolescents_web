58a26a39a8cb37baf4c9be2557898c473677d126
Sokly <hengsokly23@gmail.com>
2022-09-09 15:19:16 +0700

    App user API: add route for updating

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :visits, only: [:create]
-      resources :app_users, only: [:create]
+      resources :app_users, only: [:create, :update]
 
       get "*path" => "api#routing_error"
     end
