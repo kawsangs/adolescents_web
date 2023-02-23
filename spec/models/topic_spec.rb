c3736b26766da4e05b111c89be4b52dfb71563ed
Sokly <hengsokly23@gmail.com>
2023-02-23 13:32:05 +0700

    Topic: add name_en and rename name to name_km

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -3,16 +3,22 @@
 # Table name: topics
 #
 #  id           :uuid             not null, primary key
-#  name         :string
+#  name_km      :string
 #  version      :integer          default(0)
 #  published_at :datetime
 #  audio        :string
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #  code         :string
+#  name_en      :string
 #
 require "rails_helper"
 
 RSpec.describe Topic, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_many(:questions).dependent(:destroy) }
+  it { is_expected.to have_many(:topic_services) }
+  it { is_expected.to have_many(:services).through(:topic_services) }
+
+  it { is_expected.to validate_presence_of(:name_en) }
+  it { is_expected.to validate_presence_of(:name_km) }
 end
