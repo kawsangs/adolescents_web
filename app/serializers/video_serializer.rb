44c710803205305364ee1611bb141342eb5f69b6
Sokly <hengsokly23@gmail.com>
2023-03-07 11:50:58 +0700

    Video: add video author in serializer

--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -14,5 +14,8 @@
 #  video_batch_id    :uuid
 #
 class VideoSerializer < ActiveModel::Serializer
-  attributes :id, :name, :url, :author, :video_category, :display_order, :updated_at
+  attributes :id, :name, :url, :display_order, :updated_at
+
+  belongs_to :video_author
+  belongs_to :video_category
 end
