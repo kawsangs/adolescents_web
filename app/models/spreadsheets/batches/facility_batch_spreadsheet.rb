520a442528ed3bbad9810b5a1ccbf1f974456e22
Sokly <hengsokly23@gmail.com>
2023-07-25 14:34:24 +0700

    Facility spreadsheet: split working day to a spreadsheet

--- app/models/spreadsheets/batches/facility_batch_spreadsheet.rb
+++ app/models/spreadsheets/batches/facility_batch_spreadsheet.rb
@@ -12,7 +12,7 @@ module Spreadsheets
         items = Facility.where(id: ids).includes(:tags, :services, working_days: :working_hours)
 
         @items.map do |row|
-          item = items.select { |f| f.code == row["code"] }.first || Facility.new
+          item = items.select { |f| f.id == row["id"] }.first || Facility.new
 
           batch.importing_items.new(itemable: Spreadsheets::Batches::FacilitySpreadsheet.new(item).process(row))
         end
