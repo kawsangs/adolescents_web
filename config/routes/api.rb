12c48aaf18b95fc95f09cfdbcc8c94625e4c3cc9
Sokly <hengsokly23@gmail.com>
2023-11-23 11:23:40 +0700

    App user: add api for delete request

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :visits, only: [:create]
-      resources :app_users, only: [:create, :update]
+      resources :app_users, only: [:create, :update, :destroy]
       resources :facilities, only: [:index]
       resources :videos, only: [:index]
       resource  :mobile_tokens, only: [:create]
