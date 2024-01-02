cd47749dbf7f9f3554e4958f6b78ebd9418299ee
Sokly <hengsokly23@gmail.com>
2024-01-02 14:33:21 +0700

    Sidekiq: move mailer to background job

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module App
     config.i18n.fallbacks = [:en]
     config.i18n.available_locales = [:en, :km]
     config.i18n.default_locale = :km
+    config.active_job.queue_adapter = :sidekiq
   end
 end
