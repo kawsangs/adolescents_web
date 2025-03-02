94e9499069a5df1cd0f7115e0ac0fb00df5f8a41
hengsokly <hengsokly23@gmail.com>
2025-03-03 04:51:05 +0700

    Theme form: apply color picker

--- app/javascript/controllers/themes_controller.js
+++ app/javascript/controllers/themes_controller.js
@@ -1,8 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 import imageFile from 'commons/image_file';
 
+import $ from "jquery";
+import "jquery-minicolors";
+
 export default class extends Controller {
   connect() {
     imageFile.init();
+    this.initializeMinicolors('#theme_bg_color');
+    this.initializeMinicolors('#theme_text_color');
+    this.initializeMinicolors('#theme_button_color');
+    this.initializeMinicolors('#theme_nav_bar_color');
+  }
+
+  initializeMinicolors(domId) {
+    $(domId).minicolors({
+      theme: "bootstrap",
+      format: "hex",
+      opacity: false,
+      change: (value, opacity) => {
+        console.log("Color changed to:", value, "with opacity:", opacity);
+      },
+    });
   }
 }
